27494ea33683448f40203c8fbff7d8f434218ba2
Luiz Fernando da Silva <luiz.fernando.tech@gmail.com>
2020-08-13 20:00:56 +0100

    Good implementation, with reuse, with type safety and performance

--- src/application/Program.java
+++ src/application/Program.java
@@ -9,24 +9,34 @@ public class Program {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
-
-        System.out.println("Print Object");
-        PrintService printService = new PrintService();
+        PrintService<Integer> printServiceInteger = new PrintService<>();
 
         System.out.println("How many values?");
         int n = scanner.nextInt();
 
-        printService.addValue("Maria");
-
+        System.out.println("Print Generics <Integer>");
+        System.out.println("Type integers");
         for (int i = 0; i < n; i++) {
             int value = scanner.nextInt();
-            printService.addValue(value);
+            printServiceInteger.addValue(value);
         }
 
-        printService.print();
-        Integer first = (Integer) printService.first();
-        System.out.println("First: " + first);
+        printServiceInteger.print();
+        System.out.println("First: " + printServiceInteger.first());
 
+        System.out.println();
+
+        System.out.println("Print Generics <String>");
+        PrintService<String> printServiceString = new PrintService<>();
+
+        System.out.println("Type strings");
+        for (int i = 0; i < n; i++) {
+            String value = scanner.next();
+            printServiceString.addValue(value);
+        }
+
+        printServiceString.print();
+        System.out.println("First: " + printServiceString.first());
 
         scanner.close();
     }
